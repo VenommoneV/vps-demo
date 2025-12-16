5fccbf232d6e898116336ef18afbfe024e21d798
yzf <11305099+VenommoneV@user.noreply.gitee.com>
2025-12-16 20:43:15 +0800

    将版本号的颜色改成蓝色

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -17,7 +17,7 @@ public class DemoApplication {
     @GetMapping("/")
     public String home() {
         // 改个颜色，加个版本号，看看效果
-        return "<h1 style='color:red'>Version 2.0 Released!</h1>" + 
+        return "<h1 style='color:blue'>Version 2.0 Released!</h1>" + 
                "<p>Deployed automatically by GitHub Actions.</p>";
     }
 }
